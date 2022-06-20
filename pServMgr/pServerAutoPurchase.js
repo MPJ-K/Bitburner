18e16b1f94c629aec52727c1097210fd308eb514
MPJ / Quadryo <104721042+MPJ-K@users.noreply.github.com>
2022-06-20 23:11:31 +0200

    Update pServerAutoPurchase.js
    Code cleanup, replacing pointless vars with let or const

--- pServMgr/pServerAutoPurchase.js
+++ pServMgr/pServerAutoPurchase.js
@@ -3,18 +3,16 @@ export async function main(ns) {
 	const amount = ns.args[0];
 	const ram = ns.args[1];
 	const pServers = ns.getPurchasedServers();
-	var purchased = 0;
-	var hostNumber = 0;
-	var hostname = "tmp";
+	let purchased = 0;
+	let hostNumber = 0;
 	for (let i = 0; i < pServers.length; i++) {
-		let serv = pServers[i];
-		let servRAM = parseInt(serv.slice(7));
-		let servIndex = parseInt(serv.slice(serv.indexOf("B") + 1));
-		if (servRAM == ram && servIndex >= hostNumber) { hostNumber = servIndex + 1; }
+		const serv = pServers[i];
+		const servIndex = parseInt(serv.slice(serv.indexOf("B") + 1));
+		if (parseInt(serv.slice(7)) == ram && servIndex >= hostNumber) { hostNumber = servIndex + 1; }
 	}
 	while (purchased < amount) {
 		if (ns.getPurchasedServerCost(ram) < ns.getServerMoneyAvailable("home")) {
-			hostname = "pServer" + ram + "GB" + (hostNumber + purchased);
+			let hostname = "pServer" + ram + "GB" + (hostNumber + purchased);
 			ns.purchaseServer(hostname, ram);
 			ns.tprint("Purchased a " + ram + "GB server with hostname " + hostname + ".");
 			purchased++;
