f0d174e6e18fd2f316b807231dcf519ee48844f2
MPJ / Quadryo <104721042+MPJ-K@users.noreply.github.com>
2022-06-20 23:06:53 +0200

    Update pServerManager.js
    - Introduced formatMoney and formatBytes to improve displayed information
    - Added "mock purchase" command that allows making mock purchases to check pricing without having to remove existing servers
    - Converted unnecessary vars to let / const
    - Further code cleanup

--- pServMgr/pServerManager.js
+++ pServMgr/pServerManager.js
@@ -1,10 +1,12 @@
+import { formatMoney, formatBytes } from "/utils/helpers.js"
+
 /** @param {NS} ns **/
 export async function main(ns) {
 	const debugMode = false;
-	var maxServ = ns.getPurchasedServerLimit();
-	var currServ = ns.getPurchasedServers();
-	var cmd = "tmp";
-	const cmdList = ["list", "purchase", "remove", "exit"];
+	const maxServ = ns.getPurchasedServerLimit();
+	let currServ = ns.getPurchasedServers();
+	let cmd = "tmp";
+	const cmdList = ["list", "purchase", "mock purchase", "remove", "exit"];
 
 	ns.tprint("Welcome to the player server manager utility.");
 	ns.tprint("You currently own " + currServ.length + " server(s) out of the maximum of " + maxServ + " servers.");
@@ -19,7 +21,6 @@ export async function main(ns) {
 			else { cmd = "exit"; }
 		}
 		else {
-			maxServ = ns.getPurchasedServerLimit();
 			currServ = ns.getPurchasedServers();
 			cmd = await ns.prompt("Please select a command:", { type: "select", choices: cmdList });
 			switch (cmd) {
@@ -29,7 +30,13 @@ export async function main(ns) {
 				case "purchase":
 					var success = false;
 					while (!success) {
-						success = await purchaseManager();
+						success = await purchaseManager(false);
+					}
+					break;
+				case "mock purchase":
+					var success = false;
+					while (!success) {
+						success = await purchaseManager(true);
 					}
 					break;
 				case "remove":
@@ -41,7 +48,7 @@ export async function main(ns) {
 			}
 		}
 	}
-	ns.tprint("Exiting player server manager utility.");
+	ns.tprint("Exiting the player server manager utility.");
 
 
 	function listServers() {
@@ -55,37 +62,43 @@ export async function main(ns) {
 	}
 
 
-	async function purchaseManager() {
-		var maxPurchase = maxServ - currServ.length;
-		var listRAM = [];
+	async function purchaseManager(mock = false) {
+		const maxPurchase = maxServ - currServ.length;
+		let listRAM = [];
 		for (let i = 1; i < 21; i++) { listRAM[i] = Math.pow(2, i); }
-		if (maxPurchase <= 0) {
+		if (maxPurchase <= 0 && !mock) {
 			ns.tprint("You already own the maximum number of servers, remove some to purchase new servers.");
 			return true;
 		}
 		ns.tprint("You can purchase a maximum of " + maxPurchase + " server(s).");
-		var newServAmount = await ns.prompt("Enter the number of servers to purchase:", { type: "text" });
+		let newServAmount = await ns.prompt("Enter the number of servers to purchase:", { type: "text" });
 		newServAmount = parseInt(newServAmount);
-		if (newServAmount != newServAmount || newServAmount > maxPurchase || newServAmount < 0) {
-			ns.tprint("Error: You either did not enter a positive integer or exceeded the purchase limit.");
+		if (newServAmount != newServAmount || (newServAmount > maxPurchase && !mock) || newServAmount < 0) {
+			ns.tprint("ERROR: You either did not enter a positive integer or exceeded the purchase limit.");
 			return false;
 		}
 		if (newServAmount == 0) { return true; }
-		var newServRAM = await ns.prompt("Select the desired amount of RAM:", { type: "select", choices: listRAM });
-		var money = ns.getServerMoneyAvailable("home");
-		var newServCost = ns.getPurchasedServerCost(newServRAM) * newServAmount;
-		ns.tprint("You are about to purchase " + newServAmount + " server(s) with " + newServRAM + "GB of RAM.");
-		ns.tprint("This purchase will cost $" + newServCost + " in total.");
+		const newServRAM = await ns.prompt("Select the desired amount of RAM:", { type: "select", choices: listRAM });
+		const money = ns.getServerMoneyAvailable("home");
+		const newServCost = ns.getPurchasedServerCost(newServRAM) * newServAmount;
+		ns.tprint("You are about to purchase " + newServAmount + " server(s) with " + formatBytes(ns, newServRAM) + " of RAM.");
+		ns.tprint("This purchase will cost " + formatMoney(ns, newServCost) + " in total.");
+		if (mock) {
+			ns.tprint("Because this is a mock purchase, the command ends here.");
+			return true;
+		}
 		if (newServCost > money) {
 			ns.tprint("You do not have enough money to make this purchase.");
 			if (await ns.prompt("Wait for money to become available and automatically purchase?") && !debugMode) {
 				ns.run("/scripts/pServMgr/pServerAutoPurchase.js", 1, newServAmount, newServRAM);
 			}
 			else {
-				let maxAffordable = Math.floor((money / newServCost) * newServAmount);
+				const maxAffordable = Math.floor((money / newServCost) * newServAmount);
 				ns.tprint("You have enough money to buy " + maxAffordable + " server(s).");
-				if (await ns.prompt("Buy " + maxAffordable + " server(s) instead?")) {
-					purchaseServers(maxAffordable, newServRAM);
+				if (maxAffordable > 0) {
+					if (await ns.prompt("Buy " + maxAffordable + " server(s) instead?")) {
+						purchaseServers(maxAffordable, newServRAM);
+					}
 				}
 			}
 			return true;
@@ -99,7 +112,7 @@ export async function main(ns) {
 
 	function purchaseServers(amount, ram) {
 		if (debugMode) { return; }
-		var hostNumber = 0;
+		let hostNumber = 0;
 		for (let i = 0; i < currServ.length; i++) {
 			let serv = currServ[i];
 			let servRAM = parseInt(serv.slice(7));
@@ -109,7 +122,7 @@ export async function main(ns) {
 		for (let i = hostNumber; i < hostNumber + amount; i++) {
 			ns.purchaseServer("pServer" + ram + "GB" + i, ram)
 		}
-		ns.tprint("Purchased " + amount + " server(s) with " + ram + "GB of RAM.");
+		ns.tprint("Purchased " + amount + " server(s) with " + formatBytes(ns, ram) + " of RAM.");
 	}
 
 
@@ -117,27 +130,27 @@ export async function main(ns) {
 		if (!listServers()) {
 			return true;
 		}
-		var listTargetRAM = ["all"];
+		let listTargetRAM = ["all"];
 		for (let i = 0; i < currServ.length; i++) {
-			let servRAM = parseInt(currServ[i].slice(7)).toString();
+			const servRAM = parseInt(currServ[i].slice(7)).toString();
 			if (!listTargetRAM.includes(servRAM)) { listTargetRAM.push(servRAM); }
 		}
 		listTargetRAM.push("cancel");
-		var targetRAM = await ns.prompt("Select a RAM category for removal:", { type: "select", choices: listTargetRAM });
+		const targetRAM = await ns.prompt("Select a RAM category for removal:", { type: "select", choices: listTargetRAM });
 		if (targetRAM == "cancel") { return true; }
 		if (targetRAM == "all") {
 			if (await ns.prompt("Are you sure you would like to remove all servers?")) { removeServers("all"); }
 			return true;
 		}
-		var maxTargetAmount = 0;
+		let maxTargetAmount = 0;
 		for (let i = 0; i < currServ.length; i++) {
 			if (parseInt(currServ[i].slice(7)) == parseInt(targetRAM)) { maxTargetAmount++; }
 		}
-		ns.tprint("You own " + maxTargetAmount + " server(s) with " + targetRAM + "GB of RAM.");
-		var targetAmount = await ns.prompt("Enter the number of servers to remove:", { type: "text" });
+		ns.tprint("You own " + maxTargetAmount + " server(s) with " + formatBytes(ns, targetRAM) + " of RAM.");
+		let targetAmount = await ns.prompt("Enter the number of servers to remove:", { type: "text" });
 		targetAmount = parseInt(targetAmount);
 		if (targetAmount != targetAmount || targetAmount > maxTargetAmount || targetAmount < 0) {
-			ns.tprint("Error: The value you entered is either too large or not a positive integer.");
+			ns.tprint("ERROR: The value you entered is either too large or not a positive integer.");
 			return false;
 		}
 		if (targetAmount == 0) { return true; }
@@ -152,26 +165,26 @@ export async function main(ns) {
 		if (debugMode) { return; }
 		if (amount == "all") {
 			for (let i = 0; i < currServ.length; i++) {
-				let serv = currServ[i];
+				const serv = currServ[i];
 				ns.killall(serv);
 				ns.deleteServer(serv);
 			}
 			ns.tprint("Removed all servers.");
 		}
 		else {
-			var targetList = [];
+			let targetList = [];
 			for (let i = 0; i < currServ.length; i++) {
-				let serv = currServ[i];
+				const serv = currServ[i];
 				if (parseInt(serv.slice(7)).toString() == ram) {
 					targetList.push(serv);
 				}
 			}
 			for (let i = targetList.length - amount; i < targetList.length; i++) {
-				let serv = targetList[i];
+				const serv = targetList[i];
 				ns.killall(serv);
 				ns.deleteServer(serv);
 			}
-			ns.tprint("Removed " + amount + " server(s) with " + ram + "GB of RAM.");
+			ns.tprint("Removed " + amount + " server(s) with " + formatBytes(ns, ram) + "GB of RAM.");
 		}
 	}
 }
